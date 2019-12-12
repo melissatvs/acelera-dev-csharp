196ec05ad7c4d27e250c8973c23fbefeb6adaf60
Melissa <melissa.tv@hotmail.com>
2019-12-12 10:30:13 -0300

    add desafio5

--- README.md
+++ README.md
@@ -5,5 +5,6 @@
  * [Desafio 2 - Criptografia de Júlio César](desafio2)
  * [Desafio 3 - Os dez maiores estados brasileiros em extensão territorial](desafio3)
  * [Desafio 4 - Backend para gerenciar times de futebol](desafio4)
+ * [Desafio 5 - Processe dados dos jogadores do FIFA© 2017](desafio5)
  
  * [Exercícios da Aula 2 - POO](exercicio-poo-aula2)
---
+++ desafio5/Source/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Codenation.Challenge
+{
+    public class Player
+    {
+        public string FullName { get; set; }
+        public string Nationality { get; set; }
+        public string Club { get; set; }
+        public decimal ReleaseClause { get; set; }
+        public DateTime BirthDate { get; set; }
+        public decimal Wage { get; set; }
+        public int Age { get; set; }
+
+        public Player(string fullName, string nationality, string club, decimal releaseClause, DateTime birthDate, decimal wage, int age)
+        {
+            this.FullName = fullName;
+            this.Nationality = nationality;
+            this.Club = club;
+            this.ReleaseClause = releaseClause;
+            this.BirthDate = birthDate;
+            this.Wage = wage;
+            this.Age = age;
+        }
+    }
+}
