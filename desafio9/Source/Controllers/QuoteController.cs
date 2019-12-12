393b4ce2b5e49338e6eeee5e5cfe03876a544179
Melissa <melissa.tv@hotmail.com>
2019-12-12 13:05:39 -0300

    add desafio 9

---
+++ desafio9/Source/Controllers/QuoteController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using Codenation.Challenge.Models;
+using Codenation.Challenge.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Codenation.Challenge.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuoteController : ControllerBase
+    {
+        private readonly IQuoteService _service;
+        public QuoteController(IQuoteService service)
+        {
+            _service = service;
+        }
+
+        // GET api/quote
+        [HttpGet]
+        public ActionResult<QuoteView> GetAnyQuote()
+        {
+            try
+            {
+                Quote quote = _service.GetAnyQuote();
+
+                if (quote == null)
+                {
+                    return NotFound();
+                }
+
+                QuoteView quoteView = new QuoteView();
+
+                quoteView.Id = quote.Id;
+                quoteView.Actor = quote.Actor;
+                quoteView.Detail = quote.Detail;
+
+                return quoteView;
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        // GET api/quote/{actor}
+        [HttpGet("{actor}")]
+        public ActionResult<QuoteView> GetAnyQuote(string actor)
+        {
+            try
+            {
+                Quote quote = _service.GetAnyQuote(actor);                
+
+                if (quote == null)
+                {
+                    return NotFound();
+                }
+
+                QuoteView quoteView = new QuoteView();
+
+                quoteView.Id = quote.Id;
+                quoteView.Actor = quote.Actor;
+                quoteView.Detail = quote.Detail;
+
+                return quoteView;
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+    }
+}
